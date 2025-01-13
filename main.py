8b357aa2819a273d65d35a071ce9c7d003f8381f
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2025-01-13 23:30:00 +0100

    auto-check

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 class Animal:
-    color = "white"
-
     def __init__(self, nickname, weight):
         self.nickname = nickname
         self.weight = weight
@@ -11,13 +9,9 @@ class Animal:
     def change_weight(self, weight):
         self.weight = weight
 
-    @classmethod
-    def change_color(cls, new_color):
-        cls.color = new_color
 
-first_animal = Animal("Alex", 200)
-first_animal.change_color("red")
-second_animal = Animal("John", 150)
-second_animal.change_color("green")
+class Cat(Animal):
+    def say(self):
+        return "Meow"
 
-print(first_animal)
+cat = Cat(nickname="Simon", weight=10)
